e053fe162b28f372438ae648bb5f491be7883169
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2022-02-01 15:58:45 +0100

    Update passwordManager.sh

--- passwordManager.sh
+++ passwordManager.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! [ -f "./psswdmng/config" ] ; then #Create files
+
+    mkdir psswdmng #Create directory
+    touch ./psswdmng/config #Create config file
+    echo "Insert your mail (use the GPG mail)"
+    read mail
+    echo $mail > ./psswdmng/config
+
+fi
+
 usage="Usage: passwordManager [-r][-a][-l]"
 
 list=0
@@ -38,12 +48,19 @@ fi
 
 if [ $list -eq 1 ] ; then #List all accounts
     
-    cat accounts.txt | awk '{print NR ")" $1}'
+    if [ -f "accounts.tmp.gpg" ] ; then #Check if file exists, if not then do not decrypt
+        gpg --output accounts.tmp -d accounts.tmp.gpg
+    else
+        echo "No accounts detected"
+        exit 0
+    fi
+
+    cat accounts.tmp | awk '{print NR ")" $1}'
 
     row=""
      while [ ${#row} -eq 0 ] ; do
 
-        echo "Please enter the row"
+        echo "Please enter the row (0 to abort)"
         read row
 
         if ! [[ $row =~ ^[0-9]+$ ]] ; then #If the row is not an integer, then loop again
@@ -51,31 +68,34 @@ if [ $list -eq 1 ] ; then #List all accounts
         fi
 
         lines=0
-        lines=$(wc -l accounts.txt | awk '{print $1}')
+        lines=$(wc -l accounts.tmp | awk '{print $1}')
         if [ $row -gt $lines ] ; then #If the row is too big, then loop again
             echo "Row number too big"
             row=""
         fi
-        if [ $row -lt 1 ] ; then #If the row is too small, then loop again
-            echo "Row number too small"
-            row=""
+        if [ $row -eq 0 ] ; then #If the row is 0, then abort
+            echo "Aborting"
+            rm accounts.tmp #Delete plain text file
+            exit 0
         fi
 
     done
 
-    site=$(head --lines=$row accounts.txt | tail --lines=1 | awk '{print $1}') #Get site name
-    username=$(head --lines=$row accounts.txt | tail --lines=1 | awk '{print $2}') #Get username
-    password=$(head --lines=$row accounts.txt | tail --lines=1 | awk '{print $3}') #Get password
+    site=$(head --lines=$row accounts.tmp | tail --lines=1 | awk '{print $1}') #Get site name
+    username=$(head --lines=$row accounts.tmp | tail --lines=1 | awk '{print $2}') #Get username
+    password=$(head --lines=$row accounts.tmp | tail --lines=1 | awk '{print $3}') #Get password
 
     echo -e "\n\nSite: $site"
     echo "Username: $username"
     echo "Password: $password"
 
+    rm accounts.tmp #Delete plain text file
+
     exit 0
 fi
 
 if [ $add -eq 1 ] ; then #Add new account 
-    
+
     site=""
     username=""
     password=""
@@ -95,7 +115,16 @@ if [ $add -eq 1 ] ; then #Add new account
         read password
     done  
 
-    echo $site $username $password >>accounts.txt #Append the infos into the file
+    if [ -f "accounts.tmp.gpg" ] ; then #Check if file exists, if not then do not decrypt
+        gpg --output accounts.tmp -d accounts.tmp.gpg
+    fi
+
+    echo $site $username $password >>accounts.tmp #Append the infos into the file
+
+    rm accounts.tmp.gpg 2>/dev/null #Delete old crypted file
+    dest=$(cat ./psswdmng/config) #Get destination mail
+    gpg -r $dest -e accounts.tmp #Encrypt file
+    rm accounts.tmp #Delete plain text file
 
     exit 0
 
@@ -103,7 +132,14 @@ fi
 
 if [ $remove -eq 1 ] ; then #Remove an existing account
     
-    cat accounts.txt | awk '{print NR ")" $0}'
+    if [ -f "accounts.tmp.gpg" ] ; then #Check if file exists, if not then do not decrypt
+        gpg --output accounts.tmp -d accounts.tmp.gpg
+    else
+        echo "No accounts detected"
+        exit 0
+    fi
+
+    cat accounts.tmp | awk '{print NR ")" $0}'
 
     row=""
 
@@ -117,7 +153,7 @@ if [ $remove -eq 1 ] ; then #Remove an existing account
         fi
 
         lines=0
-        lines=$(wc -l accounts.txt | awk '{print $1}')
+        lines=$(wc -l accounts.tmp | awk '{print $1}')
         if [ $row -gt $lines ] ; then #If the row is too big, then loop again
             echo "Row number too big"
             row=""
@@ -142,7 +178,12 @@ if [ $remove -eq 1 ] ; then #Remove an existing account
         exit 0
     fi
 
-    sed -i $row'd' ./accounts.txt #Delete the selected row
+    sed -i $row'd' ./accounts.tmp #Delete the selected row
+
+    rm accounts.tmp.gpg 2>/dev/null #Delete old crypted file
+    dest=$(cat ./psswdmng/config) #Get destination mail
+    gpg -r $dest -e accounts.tmp #Encrypt file
+    rm accounts.tmp #Delete plain text file
 
     echo "Row $row deleted successfully"
 
